3552e207da2ad830f0b68a3561065fab342881e0
BlazEffect <kamalinigor@gmail.com>
2023-03-21 22:34:37 +0300

    Remove laravel sanctum and change user migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,10 +17,9 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->string('vk_user_id')->unique();
+            $table->timestamp('created_at')->nullable();
         });
     }
 
